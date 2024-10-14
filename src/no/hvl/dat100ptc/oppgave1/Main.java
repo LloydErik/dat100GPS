e1d1b31cf1eaa6e8393f3f147a76b09f1f674dbd
LloydErik <186490@stud.hvl.no>
2024-10-14 13:22:18 +0200

    Update Main.java

--- src/no/hvl/dat100ptc/oppgave1/Main.java
+++ src/no/hvl/dat100ptc/oppgave1/Main.java
@@ -4,7 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		// TODO
+		GPSPoint point = new GPSPoint(1,2.0,3.0,5.0);
+		
+		System.out.println(point.getTime());
+		System.out.println(point.getLatitude());
+		System.out.println(point.getLongitude());
+		System.out.println(point.getElevation());
+		
+		System.out.println(point.toString());
+		
+		
 		
 	}
 
